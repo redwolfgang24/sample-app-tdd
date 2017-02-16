6743aead5e2dc9aae4fb015ac07497c86fa5fd5d
Larry M. Borrero <redwolfagng20@gmail.com>
2017-02-16 22:14:54 +0800

    initialize user data.

--- src/groovy/com/sampleapptdd/demo/bootstrap/init/DataInitializer.groovy
+++ src/groovy/com/sampleapptdd/demo/bootstrap/init/DataInitializer.groovy
@@ -1,5 +1,10 @@
 package com.sampleapptdd.demo.bootstrap.init
 
+import com.sampleapptdd.demo.User
+import com.sampleapptdd.demo.security.Role
+import com.sampleapptdd.demo.type.RoleAuthority
+import com.sampleapptdd.demo.type.Status
+
 class DataInitializer implements BootstrapInitializerComponent {
 
     @Override
@@ -8,6 +13,33 @@ class DataInitializer implements BootstrapInitializerComponent {
     }
 
     private void executeDataInitializer() {
+        Role adminRole = new Role(authority: RoleAuthority.ROLE_ADMIN).save(flush: true, failOnError: true)
+        Role userRole = new Role(authority: RoleAuthority.ROLE_USER).save(flush: true, failOnError: true)
 
+        User adminUser = new User(
+                firstName: "Learning",
+                middleName: "Web",
+                lastName: "Framework",
+                username: "sampleapp",
+                password: "password",
+                emailAddress: "sampleapp@yopmail.com",
+                roles: [adminRole],
+                status: Status.ACTIVE,
+                verified: true,
+                dateVerified: new Date()
+        ).save(flush: true, failOnError: true)
+
+        User user = new User(
+                firstName: "Learning Two",
+                middleName: "Web",
+                lastName: "Framework",
+                username: "sampleapp",
+                password: "password",
+                emailAddress: "sampleapp2@yopmail.com",
+                roles: [userRole],
+                status: Status.ACTIVE,
+                verified: true,
+                dateVerified: new Date()
+        ).save(flush: true, failOnError: true)
     }
 }
