c1bb33b9c4514556fe7f940e5359e9087abd82e2
Larry M. Borrero <redwolfagng20@gmail.com>
2017-02-16 14:38:57 +0800

    update build config.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,8 +3,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
@@ -40,37 +40,87 @@ grails.project.dependency.resolution = {
         mavenLocal()
         grailsCentral()
         mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo 'http://repo.spring.io/milestone'
+        mavenRepo 'http://mvnrepository.com'
+        mavenRepo 'https://repo1.maven.org/maven2/'
     }
 
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        // runtime 'mysql:mysql-connector-java:5.1.29'
-        // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
-        test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+        build 'org.apache.httpcomponents:httpcore:4.3.2'
+        build 'org.apache.httpcomponents:httpclient:4.3.2'
+        runtime 'org.apache.httpcomponents:httpcore:4.3.2'
+        runtime 'org.apache.httpcomponents:httpclient:4.3.2'
+        runtime 'com.github.groovy-wslite:groovy-wslite:1.1.0'
+        //runtime 'org.postgresql:postgresql:9.4.1208.jre7'
+        runtime 'org.postgresql:postgresql:9.4-1206-jdbc41'
+        //runtime 'org.postgresql:postgresql:9.3-1100-jdbc4'
+
+        compile 'org.elasticmq:elasticmq-rest-sqs_2.11:0.8.12'
+        compile "org.jadira.usertype:usertype.jodatime:2.0.1"
+        compile 'com.google.guava:guava:17.0'
+        compile 'dnsjava:dnsjava:2.1.7'
+        compile 'jmimemagic:jmimemagic:0.1.2'
+        compile 'com.twelvemonkeys.imageio:imageio-ico:3.0.2'
+        compile 'org.im4java:im4java:1.4.0'
+        compile 'org.imgscalr:imgscalr-lib:4.2'
+        compile 'org.flywaydb:flyway-core:3.0'
+        compile 'org.rythmengine:rythm-engine:1.0.1'
+        compile 'org.jsoup:jsoup:1.8.1'
+        compile 'org.codehaus.groovy.modules.http-builder:http-builder:0.7.1'
+        compile "com.pusher:pusher-http-java:1.0.0"
+        compile 'com.cedarsoftware:json-io:2.7.2'   // json-io for resolvable notification serialization
+        compile 'org.apache.ws.security:wss4j:1.6.17'
+        compile 'org.apache.cxf:cxf-rt-ws-security:2.6.2'
+        compile 'org.apache.activemq:activemq-core:5.7.0'
+        compile 'org.apache.commons:commons-csv:1.2'
+        compile 'org.apache.tika:tika-core:1.12'
+        compile "org.projectlombok:lombok:1.16.2"
+        compile 'commons-io:commons-io:2.4'
+        compile 'commons-fileupload:commons-fileupload:1.3'
+        compile 'org.codehaus.gpars:gpars:1.2.1'
+        compile 'com.itextpdf.tool:xmlworker:5.5.8'
+        compile "jmimemagic:jmimemagic:0.1.2"
     }
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.55.2" // or ":tomcat:8.0.20"
+        //build ":tomcat:7.0.55"
+        build ':tomcat:8.0.30'
+        compile ":aws-sdk:1.10.44"
+        //compile ':aws-sdk:1.7.7'
 
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
-        compile ":asset-pipeline:2.1.5"
+        //compile ':asset-pipeline:1.9.9'
+        compile ':asset-pipeline:2.1.5'
+        //compile ':asset-pipeline:2.7.2'
+        compile ":mail:1.0.7"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
+        runtime ':hibernate:3.6.10.19' // or ':hibernate4:4.3.6.1'
         runtime ":database-migration:1.4.0"
-        runtime ":jquery:1.11.1"
+        runtime ':jquery:1.11.0.2'
 
-        // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.9.0"
-        //compile ":less-asset-pipeline:1.10.0"
-        //compile ":coffee-asset-pipeline:1.8.0"
-        //compile ":handlebars-asset-pipeline:1.3.0.3"
+        //test data builder plugin
+        compile ':build-test-data:2.4.0'
+
+        // test hibernate criteria
+        //test ":plastic-criteria:1.5.1"
+
+        compile ":spring-security-core:2.0-RC3"
+        compile ":spring-security-rest:1.4.1.RC1", {
+            excludes: 'spring-security-core'
+        }
+
+        compile ':gflyway2:0.4.1'
+
+        compile ':functional-test-development:0.9.3'
+        compile ':quartz:1.0.1'
+        compile ":platform-core:1.0.0"
+        compile ":jms:1.3-SNAPSHOT"
+        compile ":remote-control:1.5"
+        compile ":rest-client-builder:2.1.1"
+        compile ":joda-time:1.5"
     }
 }
