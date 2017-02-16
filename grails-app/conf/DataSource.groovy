c159b12d6f791cad54d61ad4ace05727ac969334
Larry M. Borrero <redwolfagng20@gmail.com>
2017-02-16 21:30:09 +0800

    update data source.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,37 +1,52 @@
 dataSource {
     pooled = true
     jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-//    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
-    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
+//    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
     flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
 
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
     test {
         dataSource {
-            dbCreate = "update"
+            driverClassName = "org.h2.Driver"
+            username = ""
+            password = ""
+            dbCreate = "create"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
+    development {
+        dataSource {
+            driverClassName = "org.postgresql.Driver"
+            username = "postgres"
+            password = "password"
+            dbCreate = "create-drop" /*'update'*/ // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:postgresql://localhost/sampledb"
+        }
+    }
+    staging {
+        dataSource {
+            driverClassName = "org.postgresql.Driver"
+            username = "postgres"
+            password = "password"
+            dbCreate = "update" /*'update'*/ // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:postgresql://localhost/sampledb"
         }
     }
     production {
         dataSource {
+            driverClassName = "org.postgresql.Driver"
+            username = "postgres"
+            password = "password"
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "org.postgresql.Driver"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
