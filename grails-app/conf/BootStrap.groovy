28950ee1f79947a8c8886dddb05118974da938ca
Larry M. Borrero <redwolfagng20@gmail.com>
2017-02-16 22:23:42 +0800

    update bootstrap.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,23 @@
+import com.sampleapptdd.demo.bootstrap.BootstrapInitializer
+
 class BootStrap {
 
+    BootstrapInitializer bootstrapInitializer
+
     def init = { servletContext ->
+        environments {
+            development {
+                bootstrapInitializer.execute()
+            }
+            staging {
+                bootstrapInitializer.execute()
+            }
+            production {
+                bootstrapInitializer.execute()
+            }
+        }
     }
+
     def destroy = {
     }
 }
